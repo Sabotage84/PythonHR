1c8fbc1fca2276bd84f6cf9381735b0f0e8dce37
Sabotage84 <sabotage84@mail.ru>
2020-02-11 15:25:55 +0300

    ADD NEw Year Chaos DONE

--- NewYearChaos/NewYearChaos/NewYearChaos.py
+++ NewYearChaos/NewYearChaos/NewYearChaos.py
@@ -1,27 +1,23 @@
-q=[2, 1, 5, 3, 4]
-res=0
-index=len(q)
-i=0
-s=reversed(q)
-for x in s:
-    print(s)
-    if (x!=index):
-        if (q[i+1]==index):
-            res+=1
-            t=x
-            q[i]=q[i+1]
-            q[i+1]=t
-        elif (q[i+2]==index):
-            res+=2
-            t=x
-            q[i]=q[i+2]
-            q[i+2]=q[i+1]
-            q[i+1]=x
-        else: 
-            print("Too chaotic")
-    i+=1
-    index-=1
-
-
-
-print(res)
+def minimumBribes(q):
+    res=0
+    flag=True
+    for x in range(len(q) - 1, -1, -1):
+        
+        if (q[x]!=x+1):
+            if (q[x-1]==x+1):
+                res+=1
+                t=q[x]
+                q[x]=q[x-1]
+                q[x-1]=t
+            elif (q[x-2]==x+1):
+                res+=2
+                t=q[x]
+                q[x]=q[x-2]
+                q[x-2]=q[x-1]
+                q[x-1]=t
+            else: 
+                print("Too chaotic")
+                flag=False
+                break
+    if (flag):
+        print(res)
